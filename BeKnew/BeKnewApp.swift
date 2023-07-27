66c20ef9e5fa9c0550edd67ce327cae76d12f9d8
Maksym Donets <timnirton7@gmail.com>
2023-07-27 18:36:58 +0300

    Initial Commit

---
+++ BeKnew/BeKnewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeKnewApp.swift
+//  BeKnew
+//
+//  Created by Max Donets on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BeKnewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
